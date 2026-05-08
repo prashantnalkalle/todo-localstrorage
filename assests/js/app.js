74518766c2aaddea146a3f9e621e02546f2b79e1
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-05-08 18:00:19 +0530

    work on read functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,6 +1,42 @@
 const cl = console.log;
+const todocontainer = document.getElementById('todocontainer')
+const todoform = document.getElementById('todoform')
+const todoitem = document.getElementById('todoitem')
+const addtodo = document.getElementById('addtodo')
+const updatetodo = document.getElementById('updatetodo')
 
 
+let todoArr =[]
+
+
+  // localStorage.setItem('todoArr',JSON.stringify(todoArr))
+
+// cl(JSON.parse(localStorage.getItem("todoArr")))
+
+
+todoArr = JSON.parse(localStorage.getItem('todoArr'))
+
+function templating(arr){
+  let result =``
+
+  arr.forEach(ele => {
+    
+    result +=`<li class='list-group-item d-flex justify-content-between' id='${ele.todoId}'>
+                  <strong>${ele.todoItem}</strong>
+                  <div>
+                    <i class="fa-solid fa-trash fa-2x text-primary" ></i>
+                    <i class="fa-regular fa-pen-to-square fa-2x text-danger"></i>
+                  </div>
+              </li>
+    `     
+    
+  });
+
+  todocontainer.innerHTML = result;
+}
+
+
+templating(todoArr)
 
 
 
